a8d380e7619aa67ca7d082eef26a7687df94d96b
cemmey <79742664+cemmey@users.noreply.github.com>
2022-10-10 18:01:56 +0100

    Add reverseproxy external IP

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: 'Udagram Application',
+  apiHost: 'http://a0f578c8cc2c2429e92ca5014965a3cd-1517753841.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
